becefb69fdcfd0d50446ae68ba5012324ba84956
Muhammad Qiyanu Fikkiya <muhammadqiyanu3@gmail.com>
2026-03-14 15:36:20 +0700

    fix dashboard layout

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,32 +4,34 @@ import InvoiceList from "../components/InvoiceList";
 import mockInvoices from "../data/mockInvoices";
 
 function Dashboard() {
-
   const [invoices, setInvoices] = useState(mockInvoices);
 
-  const addInvoice = (invoice) => {
-    setInvoices([...invoices, invoice]);
+  const sortLowest = () => {
+    const sorted = [...invoices].sort((a, b) => a.amount - b.amount);
+    setInvoices(sorted);
   };
 
-  const deleteInvoice = (id) => {
-    setInvoices(invoices.filter((inv) => inv.id !== id));
+  const sortHighest = () => {
+    const sorted = [...invoices].sort((a, b) => b.amount - a.amount);
+    setInvoices(sorted);
   };
 
   return (
-    <div>
+    <div style={{ padding: "30px" }}>
+      <h1>Billing Invoice Manager</h1>
 
-      <InvoiceForm
-  addInvoice={addInvoice}
-  invoices={invoices}
-/>
+      <div style={{ marginBottom: "20px" }}>
+        <button onClick={sortLowest} style={{ marginRight: "10px" }}>
+          Sort Amount Terendah
+        </button>
 
-      <div className="card">
-        <InvoiceList
-          invoices={invoices}
-          deleteInvoice={deleteInvoice}
-        />
+        <button onClick={sortHighest}>
+          Sort Amount Tertinggi
+        </button>
       </div>
 
+      <InvoiceForm invoices={invoices} setInvoices={setInvoices} />
+      <InvoiceList invoices={invoices} setInvoices={setInvoices} />
     </div>
   );
 }
